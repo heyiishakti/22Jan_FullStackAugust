e44d859d8a50a157ea5cbdf5fd332e5178193f92
heyShakti <89247195+heyiishakti@users.noreply.github.com>
2022-01-22 20:25:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+// const userRoute = require('./routes/users')
+const authRoute = require ('./routes/auth')
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL
+).then(() => console.log("Database is connected")
+).catch((err) => console.log(err)
+);
+
+app.use(express.json());
+app.use("/api/auth", authRoute)
+
+app.listen(5000, () => {
+    console.log("Backend is running");
+})
