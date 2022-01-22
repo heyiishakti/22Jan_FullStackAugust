ffa61cf3af90bc9cbee20f5e81d103c2c9731448
heyShakti <89247195+heyiishakti@users.noreply.github.com>
2022-01-22 20:24:41 +0530

    Add files via upload

---
+++ models/Cart.js
@@ -0,0 +1,15 @@
+// const mongoose = require("mongoose")
+
+// const CartSchema = new mongoose.Schema(
+//     {
+//         userId: { type: Number, required: true, unique: true },
+//         products: [
+//             {
+//                 id: {
+//                 type:String,
+//             } },]
+//     },
+//     { timestamp: true }
+// );
+
+// module.exports = mongoose.model("Product", ProductSchema)
---
+++ models/Order.js
@@ -0,0 +1,14 @@
+// const mongoose = require("mongoose")
+
+// const OrderSchema = new mongoose.Schema(
+//     {
+//         id: { type: Number, required: true, unique: true },
+//         title: { type: String, required: true, unique: true },
+//         desc: { type: string, required: true, unique: true, },
+//         img: { type: String, required: true },
+//         price: { typa: Number, required: true },
+//     },
+//     { timestamp: true }
+// );
+
+// module.exports = mongoose.model("Order", OrderSchema)
